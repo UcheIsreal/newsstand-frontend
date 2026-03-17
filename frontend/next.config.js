97f440f57e18516b3dc5912db0e498d7ee4d7866
UcheIsreal <147603233+UcheIsreal@users.noreply.github.com>
2026-03-17 02:28:09 +0100

    Add custom headers configuration to Next.js

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,3 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          { key: "X-Robots-Tag", value: "index, follow" }
+        ]
+      }
+    ];
+  }
+}
 module.exports = nextConfig
